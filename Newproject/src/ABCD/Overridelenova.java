5b79452f3916337b5aacc9084559240bfc07e9be
ASHARA1234 <ashaashashi93@gmail.com>
2023-09-26 15:02:02 +0530

    This is my javatest

---
+++ Newproject/src/ABCD/Overridelenova.java
@@ -0,0 +1,22 @@
+package ABCD;
+
+public class Overridelenova {
+	
+public void cut() {
+			System.out.println("cut code");
+		}
+		
+		public void keyboard () {
+			System.out.println("  keyboard code");
+		}
+		
+		public void paste () {
+			System.out.println("  copy code");
+		}
+		public static void main(String[] args) {
+			
+
+
+	}
+}
+
